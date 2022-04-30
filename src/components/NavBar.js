a5590b8480d48cecaea33d2e74d5dffe4ff1e35e
KateGordin <kate.gordin@gmail.com>
2022-04-30 14:01:56 +0200

    Routes, Buttons, NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./NavBar.scss";
+import event from "../assets/event.jpeg";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <NavLink end to={"/"}>
+        <img
+          className="navbar__logo"
+          alt="logo"
+          style={{ width: 170 }}
+          src={event}
+        />
+      </NavLink>
+      <div className="navbar__buttons">
+        <NavLink end to={"/events"}>
+          <button className="btn-green">Events</button>
+        </NavLink>
+
+        <NavLink end to={"/login"}>
+          <button className="btn-primary">Log in</button>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
