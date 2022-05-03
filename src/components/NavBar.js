13ff2d3595325b1a8893a2f718c1265be8cb980c
KateGordin <kate.gordin@gmail.com>
2022-05-03 13:39:09 +0200

    Modal pop-up in events

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,23 @@
 // import event from "../assets/music.jpeg";
 // import { NavLink } from "react-router-dom";
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// // export default function NavBar() {
+// //   return (
+// //     <div className="navbar">
+// //       <NavLink end to={"/"}>
+// //         <img
+// //           className="navbar__logo"
+// //           alt="logo"
+// //           style={{ width: 170 }}
+// //           src={event}
+// //         />
+// //       </NavLink>
+// //       <div className="navbar__buttons">
+// //         <NavLink end to={"/events"}>
+// //           <button className="btn-green">Events</button>
+// //         </NavLink>
+// =======
 // export default function NavBar() {
 //   return (
 //     <div className="navbar">
@@ -11,7 +27,7 @@
 //         <img
 //           className="navbar__logo"
 //           alt="logo"
-//           style={{ width: 170 }}
+//           style={{ width: 170, height: 90 }}
 //           src={event}
 //         />
 //       </NavLink>
@@ -19,23 +35,7 @@
 //         <NavLink end to={"/events"}>
 //           <button className="btn-green">Events</button>
 //         </NavLink>
-=======
-export default function NavBar() {
-  return (
-    <div className="navbar">
-      <NavLink end to={"/"}>
-        <img
-          className="navbar__logo"
-          alt="logo"
-          style={{ width: 170, height: 90 }}
-          src={event}
-        />
-      </NavLink>
-      <div className="navbar__buttons">
-        <NavLink end to={"/events"}>
-          <button className="btn-green">Events</button>
-        </NavLink>
->>>>>>> c2e7a1de7420472d3b66060c03ab2cfa18aa9c77
+// >>>>>>> c2e7a1de7420472d3b66060c03ab2cfa18aa9c77
 
 //         <NavLink end to={"/login"}>
 //           <button className="btn-primary">Log in</button>
