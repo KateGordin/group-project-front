c849e2468fbc5fc5df60e96b8ba4b50628ec103c
Risy33 <dario.risitano@gmail.com>
2022-05-03 09:58:14 +0200

    merging kate code

--- src/store/Events/actions.js
+++ src/store/Events/actions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const GET_EVENTS = "getEvents/events";
 export const LOGIN_SUCCESS = "loginSuccess/events";
+export const INCREMENT_LIKES = "incrementLikes/events";
 
 export const loginSuccess = (artistWithToken) => {
   return {
@@ -16,8 +17,27 @@ const getEvents = (events) => ({
   payload: { events },
 });
 
+const incrementLikes = (likes) => ({
+  type: INCREMENT_LIKES,
+  payload: likes,
+});
+
 export const fetchEvents = async (dispatch, getState) => {
   const response = await axios.get(`${apiUrl}/events`);
   dispatch(getEvents(response.data.events));
   console.log(response);
+};
+
+export const updateLike = (id, like) => {
+  return async (dispatch, getState) => {
+    try {
+      const response = await axios.patch(`${apiUrl}/events/${id}`, {
+        like: like + 1,
+      });
+      dispatch(fetchEvents);
+      dispatch(incrementLikes(id));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 };
