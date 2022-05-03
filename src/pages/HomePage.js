9244e1dfa31770082ac75d436e5ebdbed5a78cae
Joram3000 <98752947+Joram3000@users.noreply.github.com>
2022-05-03 13:48:20 +0200

    added button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,23 @@
 import React from "react";
 import video from "../assets/video.mp4";
 import "./HomePage.scss";
+import { NavLink } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
   return (
     <div className="home">
       <video autoPlay muted loop id="myVideo">
         <source src={video} type="video/mp4" />
       </video>
+
+      <div className="event-button" onClick={() => navigate("/events")}>
+        <button>Go To Events</button>
+     
+      </div>
+      
     </div>
   );
 }
