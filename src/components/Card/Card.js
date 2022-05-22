bffc5759fb60f910b08cd266cb764a478587b64d
KateGordin <kate.gordin@gmail.com>
2022-05-22 20:26:00 +0200

    Delete files which never use

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,13 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import { NavLink, Link } from "react-router-dom";
 import { useState } from "react";
 
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import Button from "@mui/material/Button";
 
